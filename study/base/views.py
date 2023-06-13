623b34c68b37ac3317fd11701b8a0f9d9218828b
“Troy” <40181839+TroyLouis@users.noreply.github.com>
2023-06-12 17:04:06 -0700

    added a form

--- study/base/views.py
+++ study/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Room
-
+from .forms import RoomForm
 # Create your views here.
 
 def home(request):
@@ -13,3 +13,8 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room' : room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
