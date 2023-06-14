9a9ea8f0c78ee908ce3914adb8c8971dd1ff8ef6
“Troy” <40181839+TroyLouis@users.noreply.github.com>
2023-06-13 17:03:50 -0700

    Added ability to edit

--- study/base/views.py
+++ study/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
 from .forms import RoomForm
@@ -16,5 +16,23 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
